22f3fe5da126bf5423ccb54489391737256e5e67
priya <priyapandey8884.blr@gmail.com>
2022-09-11 20:49:15 +0530

    ADDED:[Priya] added testcases for mood message in constructor

--- Mood-Analyzer/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyserTest.java
+++ Mood-Analyzer/src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyserTest.java
@@ -5,20 +5,14 @@ import org.junit.jupiter.api.Test;
 
 class MoodAnalyserTest {
 
-    /**@Test
-    public void givenMessage_WhenProper_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String result=moodAnalyser.analyzeMood("I am in sad mood");
-        Assertions.assertEquals("SAD", result);//TC 1.1
-    }**/
-
     @Test
-
-    public void givenMessage_WhenProper_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser=new MoodAnalyser();
-        String result=moodAnalyser.analyzeMood("I am in happy");
-        Assertions.assertEquals("HAPPY", result);//TC 1.2
+    public void MessageWithSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Iam in sad mood");
+        String result = moodAnalyser.analyzeMood();
+        Assertions.assertEquals("SAD", result);
     }
+
+
 
 
 
